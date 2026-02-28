344c572bca46d209153f44539406506f52a9a95a
Pedro Neves <Pedro24319245@gmail.com>
2026-02-28 14:02:26 -0300

    Add files via upload

---
+++ TiposPrimitivos.java
@@ -0,0 +1,15 @@
+public class TiposPrimitivos{
+	public static void main(String args[]){
+		byte idade = 20;
+		short temperatura = -20000;
+		int populacaoCidade = 200000;
+		long distanciaEstrelas = 9876543210L;
+		float precoUnitario = 19.99F;
+		double pi = 3.1415926535;
+		char inicial = 'P';
+		boolean ehAluno = true;
+		
+		System.out.println("Tipo Byte");
+		System.out.println("Idade: " + idade);
+	}
+}
