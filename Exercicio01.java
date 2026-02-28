259f4c3f13de3981d7aa6e11088b787961c758a8
Pedro Neves <Pedro24319245@gmail.com>
2026-02-28 15:54:41 -0300

    Add files via upload

---
+++ Exercicio01.java
@@ -0,0 +1,18 @@
+// Nesse programa vamos imprimir os dados de um produto.
+
+public class Exercicio01{
+	public static void main(String args[]){
+		String produto = "Bolacha de Água e Sal";
+		String categoria = "Alimentos";
+		int quant =  150;
+		float preco = 5.49F;
+		
+		String venda = "\nVocê comprou: " + produto + ".";
+		venda = venda + "\nCategoria: " + categoria + ".";
+		venda = venda + "\nQuantidade: " + quant + ".";
+		venda = venda + "\nValor Unitario: R$ " + preco + ".";
+		venda = venda + "\nPreço Total: R$ " + quant * preco + ".";
+		
+		System.out.println(venda);
+		}
+}
