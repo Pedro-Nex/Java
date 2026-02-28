13cafd052c5853cd9b8ad0be24806391bf9f8c75
Pedro Neves <Pedro24319245@gmail.com>
2026-02-28 14:15:03 -0300

    Add files via upload

--- TiposPrimitivos.java
+++ TiposPrimitivos.java
@@ -9,7 +9,32 @@ public class TiposPrimitivos{
 		char inicial = 'P';
 		boolean ehAluno = true;
 		
+		System.out.println("==============================");
+		System.out.println("");
 		System.out.println("Tipo Byte");
 		System.out.println("Idade: " + idade);
+		System.out.println("");
+		System.out.println("Tipo Short");
+		System.out.println("Temperatura: " + temperatura);
+		System.out.println("");
+		System.out.println("Tipo Int");
+		System.out.println("População da Cidade: " + populacaoCidade);
+		System.out.println("");
+		System.out.println("Tipo Long");
+		System.out.println("Distacia das Estrelas: " + distanciaEstrelas);
+		System.out.println("");
+		System.out.println("Tipo Float");
+		System.out.println("Preço Unitário: " + precoUnitario);
+		System.out.println("");
+		System.out.println("Tipo Double");
+		System.out.println("Número PI: " + pi);
+		System.out.println("");
+		System.out.println("Tipo Char");
+		System.out.println("Inicial do Nome: " + inicial);
+		System.out.println("");
+		System.out.println("Tipo Boolean");
+		System.out.println("Você é aluno? " + ehAluno);
+		System.out.println("");
+		System.out.println("==============================");
 	}
 }
