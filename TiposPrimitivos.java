fe5bd150acc5366750c91a7007ffadea3e9f9291
Pedro Neves <Pedro24319245@gmail.com>
2026-02-28 14:58:33 -0300

    Add files via upload

--- TiposPrimitivos.java
+++ TiposPrimitivos.java
@@ -1,13 +1,21 @@
+/* Esse programa demonstra a declaração e o uso dos
+tipos primitivos em Java.
+Eles são a base para armazenar dados simples e eficientes.
+*/
+
 public class TiposPrimitivos{
 	public static void main(String args[]){
+		// Tipos de números inteiros.
 		byte idade = 20;
 		short temperatura = -20000;
 		int populacaoCidade = 200000;
-		long distanciaEstrelas = 9876543210L;
-		float precoUnitario = 19.99F;
+		long distanciaEstrelas = 9876543210L; // Sufixo L é obrigatório.
+		// Tipos de números decimais (ponto flutuante).
+		float precoUnitario = 19.99F; // Sufixo F é obrigatório.
 		double pi = 3.1415926535;
+		// Tipos para caracter e lógico.
 		char inicial = 'P';
-		boolean ehAluno = true;
+		boolean ehAluno = false;
 		
 		System.out.println("==============================");
 		System.out.println("");
@@ -33,8 +41,16 @@ public class TiposPrimitivos{
 		System.out.println("Inicial do Nome: " + inicial);
 		System.out.println("");
 		System.out.println("Tipo Boolean");
-		System.out.println("Você é aluno? " + ehAluno);
+		// Operador ternário.
+		// Variável_bolean ? resposta_verdadeira : resposta_falsa.
+		System.out.println(ehAluno ? "Ele é aluno." : "Ele não é aluno.");
 		System.out.println("");
 		System.out.println("==============================");
+		
+		/*
+		Comentarios em varias linha.
+		*/
+		
+		//Comentarios em linha.
 	}
 }
