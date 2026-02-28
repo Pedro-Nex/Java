1bf91ad97995bdbeecebf9218fdb2ee4029eedc1
Pedro Neves <Pedro24319245@gmail.com>
2026-02-28 12:30:54 -0300

    Add files via upload

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,9 @@
+class PrimeiroPrograma{
+	public static void main(String args[]){
+		System.out.println("====================");
+		System.out.println("Olá Mundo!!!");
+		System.out.print("Nome: ");
+		System.out.println("Pedro");
+		System.out.println("====================");
+	}
+}
