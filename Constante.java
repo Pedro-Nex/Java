27a30787689a518c47ab2c4d8be862a1ec1c9144
Pedro Neves <Pedro24319245@gmail.com>
2026-03-07 12:20:03 -0300

    Add files via upload

--- Constante.java
+++ Constante.java
@@ -7,7 +7,7 @@ public class Constante {
         final int JUROS = 10;
         final String CAPITAL_BRASIL = "Brásilia";
 
-        System.out.print("=".repeat(40));
+        System.out.print("=".repeat(50));
 
         System.out.println("\n\nCalculando o raio de um círculo.");
         double area = 15;
@@ -15,15 +15,19 @@ public class Constante {
 
         System.out.println("O raio do círculo de area " + area + " é " + String.format("%.2f", raio) + "\n");
 
-        System.out.print("=".repeat(40)); //Repete o valor entre aspas
+        System.out.print("=".repeat(50)); //Repete o valor entre aspas
 
         System.out.println("\n\nCalculando o Juros simples de uma dívida.");
         double divida = 5000.00;
         double valorJuros = divida * JUROS / 100;
         double dividaComJuros = divida + valorJuros;
 
-        System.out.println("Você está devendo: R$ " + String.format("%.2f", dividaComJuros) + "\n");
+        System.out.println("Você está devendo: R$ " + String.format("%,.2f", dividaComJuros) + "\n");
 
-        System.out.print("=".repeat(40));
+        System.out.println("=".repeat(50));
+
+        System.out.println("\nA Capital do Brasil é: " + CAPITAL_BRASIL + "\n");
+
+        System.out.println("=".repeat(50));
     }
 }
