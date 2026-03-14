f5526672aa01d4f65ff26dc360dbfae75c91b73f
Pedro Neves <Pedro24319245@gmail.com>
2026-03-14 17:52:45 -0300

    Add files via upload

--- OperadoresLogicos2.java
+++ OperadoresLogicos2.java
@@ -10,9 +10,9 @@ public class OperadoresLogicos2 {
         System.out.print("Digite o processador do computador: ");
         String processador = scan.next();
 
-        if(memoria >= 16 && processador.equalsIgnoreCase("i7") || processador.equalsIgnoreCase("i9")){
+        if(memoria >= 16 && (processador.equalsIgnoreCase("i7") || processador.equalsIgnoreCase("i9"))){
             System.out.println("Requisitos recomendados atendidos: Desempenho Ultra.");
-        }else if(memoria >= 8 && processador.equalsIgnoreCase("i5") || processador.equalsIgnoreCase("i7") || processador.equalsIgnoreCase("i9")){
+        }else if(memoria >= 8 && (processador.equalsIgnoreCase("i5") || processador.equalsIgnoreCase("i7") || processador.equalsIgnoreCase("i9"))){
             System.out.println("Requisito minimos atendidos: Desempenho Médio.");
         }else {
             System.out.println("Hardware Insuficiente para esse Software.");
